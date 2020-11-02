2b2b3d0b7c56976618ffd9ce6fee302d9a7b5472
Rami Ertimo <rami.ertimo@gmail.com>
2020-11-02 07:34:57 +0200

    Ihmeilmat blog links fis

--- src/books/IhmeIlmat.vue
+++ src/books/IhmeIlmat.vue
@@ -46,10 +46,12 @@ const quotes = [
     },
     {   
         "text": "Ihme ilmat! on siis erinomaisesti kuvitettu, selkeä tietopaketti, joka sopii hyvin myös aikuisen ja lapsen yhdessä tutkittavaksi. Mikäli aihe yhtään kiinnostaa, kirja kannattaa kyllä ehdottomasti ottaa lukulistalle.",
+        "url": "https://www.kirjavinkit.fi/arvostelut/ihme-ilmat-miksi-ilmasto-muuttuu/",
         "urlText": "Kirjavinkit, 29.11.2019"
     } ,
     {   
         "text": "Tietokirja vastaa lasten kiperiin kysymyksiin ilmastonmuutoksesta ja kertoo, mitä sille voi tehdä. Maantieteilijä ja tietokirjailija Laura Ertimon teksti saa tukea viime vuonna Oksi-sarjakuvalla ihastuttaneen Mari Ahokoivun kauniista kuvituksesta.",
+        "url": "https://www.hs.fi/kulttuuri/art-2000006246737.html",
         "urlText": "Arla Kanerva Helsingin Sanomat, 22.9.2019"
     }, 
     {
@@ -63,6 +65,7 @@ const quotes = [
     },
     {
         "text":"Ajankohtaisempaa kirjaa ei oikeastaan voi olla. Ihme Ilmat! tarttuu kuumaan aiheeseen sarjakuvalla höystetyn tietokirjan muodossa. Kirja käsittelee aihettaan kattavasti. Onkin kunnioitettava suoritus, että neljällekymmenelleseitsemälle sivulle on saatu puristettua niin paljon tietoa aiheesta ilman, että tieto vaikuttaisi väkisin kansiensa väliin ahdetulta. On vaikeaa keksiä mitä kirjaan olisi vielä voinut lisätä.",
+        "url": "https://mutsimedia.fi/lapsen-kanssa/ihme-ilmat-kasittelee-ilmastonmuutosta-seikkailun-ja-tietopaketin-avulla/?fbclid=IwAR3tFCipvGL2sXQuEF-DNc8UC2JUOxv-QyDJ5hbEN_6SXzQE6pQYAflhhiw",
         "urlText": "Mutsimedia, 3.11.2019"
     }, 
     {
@@ -71,14 +74,17 @@ const quotes = [
     },
     {
         "text":"Laura Ertimon ja Mari Ahokoivun tietokirja Ihme ilmat! tarjoaa koululaiselle ja miksei aikuisellekin tuhdin tietopaketin ilmastomuutoksen syistä ja seurauksista helposti nieltävässä paketissa.",
+        "url": "https://rouvaeriksson.blogspot.com/2019/11/ihme-ilmat-kertoo-ilmastonmuutoksesta.html?m=1&fbclid=IwAR1vkZS81AlTcadlfNDOdjlGyHBaO_goRPYoEoRq6Yys8M0cYnMLWIWuE4U",
         "urlText": "Rouvaeriksson-kirjablogi, 27.11.2019"
     },
     {
         "text":"Laura Ertimon &  Mari Ahokoivun Ihme ilmat! – Miksi ilmasto muuttuu tulisi mielestäni ottaa huomioon Tietokirjallisuuden Finlandiaa jaettaessa.",
+        "url": "https://lastenkirjahylly.blogspot.com/2019/11/lastenkirjahyllyn-varjolistalla-suuria.html",
         "urlText": "Lastenkirjahylly-blogi, Varjo-Finlandia, 4.11.2019"
     },
     {
         "text":"Ihme ilmat! – Miksi ilmasto muuttuu -kirja on toteutettu kunnianhimoisesti. Se on ulkonäöltään kaunis ja raikas. Tekstisisällöt on kirjoitettu huolella faktoihin nojaten mutta silti niin popularisoiden, että uskon lastenkin jaksavan niitä lukea ja kuunnella.",
+        "url": "http://www.kirsinkirjanurkka.fi/2019/11/laura-ertimo-ja-mari-ahokoivu-ihme.html",
         "urlText": "Kirsin kirjablogi, 17.11.2019"
     },
     {
@@ -87,6 +93,7 @@ const quotes = [
     },
     {
         "text":"Ihme ilmat! – Miksi ilmasto muuttuu on hieno kirja. Kirja menee mielestäni hienosti oppikirjana ja vieläpä sellaisena, joka jaksaa kiinnostaa lasta. Mahtavia kuvia ja paljon asiaa mukavassa muodossa.",
+        "url": "https://mannilainen.blogspot.com/2019/11/laura-ertimo-mari-ahokoivu-ihme-ilmat.html",
         "urlText": "Mannilainen-kirjablogi, 1.11.2019"
     }
 ];
